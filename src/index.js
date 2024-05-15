beedc028c5b20f5b249fdbd885a7161f450ae97f
dumbus <dumbusx@gmail.com>
2024-05-15 03:47:24 +0300

    refactor: update import of App for index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import './styles/_global.scss';
 
-import App from './App';
+import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
